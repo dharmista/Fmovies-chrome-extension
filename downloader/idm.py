1d67db610fa3d66750f73787251dda8ad3697a26
kambhampati dharmista <kambhampati dharmista>
2017-08-11 17:08:30 +0530

    Added multi threaded downloader

--- downloader/idm.py
+++ downloader/idm.py
@@ -1,15 +1,27 @@
 import os, sys
+import threading
+
 try:
     from pySmartDL import SmartDL
 except Exception as e:
     os.popen("pip install pySmartDL")
+    exit(0)
 
+#Set your own download path
 DOWNLOAD_PATH = "c:\\batman\\Downloads"
+"""
+    Keep this based on the size of file u download.
+    Size (directly proportional to) thread count
+"""
+THREADS_TO_SPLIT = 5
 
 def download(url, path, filename, serial):
 
-    obj = SmartDL(url, path + "\\"+filename+" episode {}- .mp4".format(serial))
+    obj = SmartDL(url, path + "\\"+filename+" episode {}- .mp4".format(serial), progress_bar=False, threads = THREADS_TO_SPLIT)
+
     obj.start()
+
+    print("Downloaded successfully episode {}".format(serial))
 
 if __name__ == '__main__':
     if(len(sys.argv) != 3):
@@ -21,7 +33,9 @@ if __name__ == '__main__':
     index = 0
 
     for file in f.readlines():
-        # or '~/batman/Downloads/' on linux
-        download(file, DOWNLOAD_PATH, filename, index)
+        #'~/batman/Downloads/' on linux
+        thread = threading.Thread(target=download, args=(file, DOWNLOAD_PATH, filename, index))
+        thread.start()
         index+=1
     f.close()
+    print("Working on the downloads.. Downloading parallel'y. Leave this window behind")
